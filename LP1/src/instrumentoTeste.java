92d238e976c34d9b9f76efa9d826cde5074e7fe2
Ramon Fernandes <60863640+moonees@users.noreply.github.com>
2020-09-09 23:42:14 -0300

    Update instrumentoTeste.java

--- LP1/src/instrumentoTeste.java
+++ LP1/src/instrumentoTeste.java
@@ -2,11 +2,11 @@
 public class instrumentoTeste {
 	public static void main(String [] args) {
 		instrumentoMusical[] instrumento = new instrumentoMusical[5];
-		instrumento[0] = new instrumentoMusical("Violão","gabi","Marrom");
-		instrumento[1] = new instrumentoMusical("Guitarra", "Batata", "Branca");
-		instrumento[2] = new instrumentoMusical("Saxofone", "Batata", "Dourado");
-		instrumento[3] = new instrumentoMusical("Bateria", "Batata", "Preta e Branco");
-		instrumento[4] = new instrumentoMusical("Triangulo", "Batata", "Prata");
+		instrumento[0] = new instrumentoMusical("Violão","Vintage","Marrom");
+		instrumento[1] = new instrumentoMusical("Guitarra", "Tama", "Branca");
+		instrumento[2] = new instrumentoMusical("Saxofone", "Boss", "Dourado");
+		instrumento[3] = new instrumentoMusical("Bateria", "Vintage", "Preta e Branco");
+		instrumento[4] = new instrumentoMusical("Triangulo", "Laney", "Prata");
 		
 		for(int i=0; i <5; i++) {
 			System.out.println("O instrumento é"+"\t" + instrumento[i].getNome() +"\t" + "de cor" + "\t" + instrumento[i].getCor());
