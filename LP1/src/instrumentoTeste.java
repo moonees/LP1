baef0cefdfe1b91bf9caab86556918c14ce5209e
Ramon Fernandes <60863640+moonees@users.noreply.github.com>
2020-09-09 23:40:31 -0300

    Update instrumentoTeste.java

--- LP1/src/instrumentoTeste.java
+++ LP1/src/instrumentoTeste.java
@@ -2,14 +2,14 @@
 public class instrumentoTeste {
 	public static void main(String [] args) {
 		instrumentoMusical[] instrumento = new instrumentoMusical[5];
-		instrumento[0] = new instrumentoMusical("Viol„o","Batata","Marrom");
+		instrumento[0] = new instrumentoMusical("Viol√£o","gabi","Marrom");
 		instrumento[1] = new instrumentoMusical("Guitarra", "Batata", "Branca");
 		instrumento[2] = new instrumentoMusical("Saxofone", "Batata", "Dourado");
 		instrumento[3] = new instrumentoMusical("Bateria", "Batata", "Preta e Branco");
 		instrumento[4] = new instrumentoMusical("Triangulo", "Batata", "Prata");
 		
 		for(int i=0; i <5; i++) {
-			System.out.println("O instrumento È"+"\t" + instrumento[i].getNome() +"\t" + "de cor" + "\t" + instrumento[i].getCor());
+			System.out.println("O instrumento √©"+"\t" + instrumento[i].getNome() +"\t" + "de cor" + "\t" + instrumento[i].getCor());
 			
 		}
 		instrumento[0].tocar();
